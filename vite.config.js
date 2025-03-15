0af39d29d71bd6f22c53efae91d42853bca2f271
Abah Gideon <abahgideon111@gmail.com>
2025-03-15 18:29:16 +0100

    sitemap install

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    new SitemapPlugin({ base: "https://gandoki.netlify.app", paths: ["/"] }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
