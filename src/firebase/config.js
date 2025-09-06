6b47d68de65f21305be4f999d744dee80fb1ed2d
Abah Gideon <abahgideon111@gmail.com>
2025-09-06 15:51:21 +0100

    updated security

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,13 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAVdQ-ThpMNe-73SHRrjLPl4nYUh3L5UqI",
-    authDomain: "portfolio-c24e0.firebaseapp.com",
-    projectId: "portfolio-c24e0",
-    storageBucket: "portfolio-c24e0.firebasestorage.app",
-    messagingSenderId: "775339744045",
-    appId: "1:775339744045:web:d7fc692212a0d3ce7d11a4",
-    measurementId: "G-CK17TQ8GFR"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
